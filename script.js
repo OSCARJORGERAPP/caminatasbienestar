523a1c18c8385110ffbb51b7b2a9a58961c9e719
OSCARJORGERAPP <ojrapp@gmail.com>
2026-09-18 23:52:36 -0300

    Update WhatsApp number in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const whatsappNumber = '5491137019068'; // REEMPLAZAR con tu número (ej: 54911xxxxxxx)
+  const whatsappNumber = '5491123098313'; // REEMPLAZAR con tu número (ej: 54911xxxxxxx)
 
   const waText = encodeURIComponent(
     'Hola, quiero reservar la sesión piloto de Caminatas de acompañamiento.\n' +
